cdfa29473048f895337adb426df7e22f5ddf2a70
Rick Hall <rphall@choicemaker.com>
2015-08-20 08:46:31 -0400

    First compilable version of ChunkRecordIndexSet
    Replaced core.Constant with core.EXTERNAL_DATA_FORMAT
    Replaced ChunkRecordID{Source,Sink} with ChunkRecordId{Source,Sink} (ID => Id)
    Replaced IRecordIDTranslator3 with ImmutableRecordIdTranslator
    Replace import of com.choicemaker.cm.core.util.* with com.choicemaker.util.*
    Reformatted

--- 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.io.blocking.automated.offline/src/main/java/com/choicemaker/cm/io/blocking/automated/offline/impl/ChunkRecordIndexSet.java
+++ 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.io.blocking.automated.offline/src/main/java/com/choicemaker/cm/io/blocking/automated/offline/impl/ChunkRecordIndexSet.java
@@ -11,16 +11,16 @@ import java.util.Arrays;
 import java.util.BitSet;
 
 import com.choicemaker.cm.core.BlockingException;
-import com.choicemaker.cm.core.util.LongArrayList;
-import com.choicemaker.cm.core.util.Precondition;
-import com.choicemaker.cm.io.blocking.automated.offline.core.Constants;
-import com.choicemaker.cm.io.blocking.automated.offline.core.IChunkRecordIDSource;
+import com.choicemaker.util.LongArrayList;
+import com.choicemaker.util.Precondition;
+import com.choicemaker.cm.io.blocking.automated.offline.core.EXTERNAL_DATA_FORMAT;
+import com.choicemaker.cm.io.blocking.automated.offline.core.IChunkRecordIdSource;
 import com.choicemaker.cm.io.blocking.automated.offline.core.IChunkRecordIndexSet;
-import com.choicemaker.cm.io.blocking.automated.offline.core.IRecordIDTranslator3;
+import com.choicemaker.cm.io.blocking.automated.offline.core.ImmutableRecordIdTranslator;
 
 /**
  * An implementation of IChunkRecordIndexSet that uses
- * {@link com.choicemaker.cm.io.blocking.automated.offline.impl.ChunkRecordIDSource ChunkRecordIDSource}.
+ * {@link com.choicemaker.cm.io.blocking.automated.offline.impl.ChunkRecordIdSource ChunkRecordIdSource}.
  * @author rphall
  * @version $Revision$ $Date$
  */
@@ -38,15 +38,16 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	 * which storage will expand whose the capacity of the backing storage is
 	 * exceeded.
 	 */
-	public static final int DEFAULT_CAPACITY_INCREMENT = DEFAULT_INITIAL_CAPACITY;
+	public static final int DEFAULT_CAPACITY_INCREMENT =
+		DEFAULT_INITIAL_CAPACITY;
 
-	private static final String errorMsg1 = "type must be " + Constants.STRING + " or " + Constants.BINARY;
-	private static boolean precondition1(int type) {
-		boolean retVal = (type == Constants.STRING || type == Constants.BINARY);
-		return retVal;
-	}
+	private static final String errorMsg1 = "type must be "
+			+ EXTERNAL_DATA_FORMAT.STRING + " or "
+			+ EXTERNAL_DATA_FORMAT.BINARY;
 
-	private static final String errorMsg2 = "A set must be opened before it is used.";
+	private static final String errorMsg2 =
+		"A set must be opened before it is used.";
+
 	private static boolean precondition2(long[] data) {
 		boolean retVal = (data != null);
 		return retVal;
@@ -63,7 +64,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 		if (retVal) {
 			// Data must be sorted in strictly ascending order.
 			// First value must >= IRecordIDTranslator3.MINIMUM_VALID_INDEX
-			long previous = IRecordIDTranslator3.MINIMUM_VALID_INDEX - 1;
+			long previous = ImmutableRecordIdTranslator.MINIMUM_VALID_INDEX - 1;
 			for (int i=0; i<data.length; i++) {
 				long current = data[i];
 				if (previous >= current) {
@@ -99,17 +100,17 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 		}
 	}
 
-	/** The fileName of the ChunkRecordIDSource that backs this set */
+	/** The fileName of the ChunkRecordIdSource that backs this set */
 	private final String fileName;
 
-	/** The type of the ChunkRecordIDSource that backs this set */
-	private final int type;
+	/** The type of the ChunkRecordIdSource that backs this set */
+	private final EXTERNAL_DATA_FORMAT type;
 	
 	/** A flag indicating whether diagnostics are enabled */
 	private final boolean isDebugEnabled;
 
 	/**
-	 * <em>Sorted</em> data from the ChunkRecordIDSource that backs this set.
+	 * <em>Sorted</em> data from the ChunkRecordIdSource that backs this set.
 	 * This field also acts a flag.  If this set is open, it is non-null; otherwise
 	 * it is null.
 	 */
@@ -120,7 +121,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	 * This field should always be non-null, but its value varies depending
 	 * on whether this set is open or not.
 	 */
-	private IChunkRecordIDSource source;
+	private IChunkRecordIdSource source;
 	
 	/**
 	 * Bitmap of indices that <strong><em>have</em></strong> been checked.
@@ -128,13 +129,13 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	 */
 	private BitSet isChecked;
 	
-	public ChunkRecordIndexSet (String fileName, int type, boolean isDebugEnabled) {
+	public ChunkRecordIndexSet (String fileName, EXTERNAL_DATA_FORMAT type, boolean isDebugEnabled) {
 		Precondition.assertNonEmptyString(fileName);
-		Precondition.assertBoolean(errorMsg1,precondition1(type));
+		Precondition.assertNonNullArgument(errorMsg1,type);
 		this.fileName = fileName;
 		this.type = type;
 		this.isDebugEnabled = isDebugEnabled;
-		this.source = new ChunkRecordIDSource(this.fileName,this.type);
+		this.source = new ChunkRecordIdSource(this.fileName,this.type);
 	}
 
 	/* (non-Javadoc)
@@ -153,7 +154,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	/* (non-Javadoc)
 	 * @see com.choicemaker.cm.io.blocking.automated.offline.core.IChunkRecordIndexSet#getSource()
 	 */
-	public IChunkRecordIDSource getSource() {
+	public IChunkRecordIdSource getSource() {
 		return this.source;
 	}
 
@@ -172,7 +173,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 		int capacity = DEFAULT_INITIAL_CAPACITY;
 		LongArrayList list = new LongArrayList(capacity);
 
-		IChunkRecordIDSource src = this.getSource();
+		IChunkRecordIdSource src = this.getSource();
 		src.open();
 
 		int count = 0;
@@ -182,7 +183,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 				capacity += DEFAULT_INITIAL_CAPACITY;
 				list.ensureCapacity(capacity);
 			}
-			long index = src.getNext();
+			long index = src.next();
 			list.add(index);
 		}
 		this.data = list.toArray();
@@ -208,7 +209,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	public void close() throws BlockingException {
 		this.data = null;
 		this.getSource().close();
-		this.source = new ChunkRecordIDSource(this.fileName,this.type);
+		this.source = new ChunkRecordIdSource(this.fileName,this.type);
 	}
 
 	/* (non-Javadoc)
@@ -225,7 +226,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 		if (this.data != null) {
 			this.close();
 		}
-		this.getSource().remove();
+		this.getSource().delete();
 	}
 
 	/* (non-Javadoc)
@@ -238,8 +239,8 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 	/* (non-Javadoc)
 	 * @see com.choicemaker.cm.io.blocking.automated.offline.core.IChunkRecordIndexSet#isDebugEnabled()
 	 */
-	public IChunkRecordIDSource getUncheckedIndices() {
-		IChunkRecordIDSource retVal;
+	public IChunkRecordIdSource getUncheckedIndices() {
+		IChunkRecordIdSource retVal;
 		synchronized(this) {
 			int countUnchecked = this.isChecked.size() - this.isChecked.cardinality();
 			LongArrayList unchecked = new LongArrayList(countUnchecked);
@@ -256,15 +257,15 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 		return retVal;
 	}
 	
-	private static IChunkRecordIDSource createIndexSource(final long[] indices) {
+	private static IChunkRecordIdSource createIndexSource(final long[] indices) {
 		final long[] copy;
 		synchronized (indices) {
 			copy = new long[indices.length];
 			System.arraycopy(indices,0,copy,0,indices.length);
 		}
-		IChunkRecordIDSource retVal = new IChunkRecordIDSource() {
+		IChunkRecordIdSource retVal = new IChunkRecordIdSource() {
 			private int current = 0;
-			public long getNext() throws BlockingException {
+			public Long next() throws BlockingException {
 				if (current >= copy.length) {
 					throw new ArrayIndexOutOfBoundsException(
 						"current: " + current + "; length: " + copy.length);
@@ -290,7 +291,7 @@ public class ChunkRecordIndexSet implements IChunkRecordIndexSet {
 				return null;
 			}
 
-			public void remove() throws BlockingException {
+			public void delete() throws BlockingException {
 			}
 		};
 		return retVal;
