00c92245aa2235fb980fd8234209dac9a024e222
Rick Hall <rphall@choicemaker.com>
2016-02-11 00:49:53 -0500

    Stubbed unit test for LogPartitionerApp

---
+++ 2.7.x/misc/logfrequencypartitioner-app/src/main/java/com/choicemaker/cm/logfrequencypartitioner/app/LogPartitionerAppTest.java
@@ -0,0 +1,44 @@
+package com.choicemaker.cm.logfrequencypartitioner.app;
+
+//import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LogPartitionerAppTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreatePartitions() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadInput() {
+//		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteOutput() {
+//		fail("Not yet implemented");
+	}
+
+}
