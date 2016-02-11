229d3a0cdcf793977cac8ddd2d6ff7fb15533394
Rick Hall <rphall@choicemaker.com>
2016-02-11 16:23:10 -0500

    Working unit test for LogPartitionerApp

--- 2.7.x/misc/logfrequencypartitioner-app/src/test/java/com/choicemaker/cm/logfrequencypartitioner/app/LogPartitionerAppTest.java
+++ 2.7.x/misc/logfrequencypartitioner-app/src/test/java/com/choicemaker/cm/logfrequencypartitioner/app/LogPartitionerAppTest.java
@@ -1,44 +1,263 @@
 package com.choicemaker.cm.logfrequencypartitioner.app;
 
-//import static org.junit.Assert.*;
+import static com.choicemaker.cm.logfrequencypartitioner.app.LogPartitionerFileFormat.*;
+import static com.choicemaker.cm.logfrequencypartitioner.app.TestUtils.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.choicemaker.util.LogFrequencyPartitioner.ValueCount;
+import com.choicemaker.util.LogFrequencyPartitioner.ValueRank;
+
 public class LogPartitionerAppTest {
+
+	private static final Logger logger = Logger
+			.getLogger(LogPartitionerAppTest.class.getName());
+
+	private static final char[] fieldDelimiters = new char[] {
+			',', '|' };
+
+	private static final String[] lineDelimiters = new String[] {
+			"\r\n", "\n", "\r" };
+
+	private static List<ValueCount> valueCounts;
+	private static List<ValueRank> expected;
+	private static int numPartitions;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		numPartitions = 4;
+
+		final double sqrt10 = Math.pow(10., 0.5); // 3.162...
+		List<ValueCount> counts = new ArrayList<>();
+		counts.add(new ValueCount("value 1", 1));
+		counts.add(new ValueCount("value 3", (int) sqrt10));
+		counts.add(new ValueCount("value 4", 1 + (int) sqrt10));
+		counts.add(new ValueCount("value 10", 10));
+		counts.add(new ValueCount("value 31", (int) (10. * sqrt10)));
+		counts.add(new ValueCount("value 32", 1 + (int) (10. * sqrt10)));
+		counts.add(new ValueCount("value 100", 100));
+		counts.add(new ValueCount("value 101", 100));
+		counts.add(new ValueCount("value 1000", 100));
+		valueCounts = Collections.unmodifiableList(counts);
+
+		List<ValueRank> ranks = new ArrayList<>();
+		ranks.add(new ValueRank("value 1", 0));
+		ranks.add(new ValueRank("value 3", 0));
+		ranks.add(new ValueRank("value 4", 0));
+		ranks.add(new ValueRank("value 10", 1));
+		ranks.add(new ValueRank("value 31", 1));
+		ranks.add(new ValueRank("value 32", 2));
+		ranks.add(new ValueRank("value 100", 3));
+		ranks.add(new ValueRank("value 101", 3));
+		ranks.add(new ValueRank("value 1000", 3));
+		expected = Collections.unmodifiableList(ranks);
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	void testCreatePartitions(LogPartitionerFileFormat inputFormat,
+			char inputFieldDelimiter, String inputLineDelimiter,
+			LogPartitionerFileFormat outputFormat, char outputFieldDelimiter,
+			String outputLineDelimiter) {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+		final boolean isHelp = false;
+		final List<String> errors = null;
+		File inputFile = null;
+		File outputFile = null;
+		String inputFileName = null;
+		String outputFileName = null;
+		try {
+			inputFile = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
+			inputFileName = inputFile.getAbsolutePath();
+			inputFile.delete();
+			inputFile = null;
 
-	@After
-	public void tearDown() throws Exception {
+			outputFile = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
+			outputFileName = outputFile.getAbsolutePath();
+			outputFile.delete();
+			outputFile = null;
+
+			LogPartitionerApp.writeOutput(valueCounts, inputFileName,
+					inputFormat, inputFieldDelimiter, inputLineDelimiter);
+			LogPartitionerParams appParams =
+				new LogPartitionerParams(isHelp, errors, inputFileName,
+						inputFormat, inputFieldDelimiter, inputLineDelimiter,
+						outputFileName, outputFormat, outputFieldDelimiter,
+						outputLineDelimiter, numPartitions);
+			final LogPartitionerApp app = new LogPartitionerApp(appParams);
+			final int count = app.createPartitions();
+			assertTrue(count == valueCounts.size());
+
+			List<ValueRank> computed;
+			if (outputFormat == ALT_LINES) {
+				computed =
+					readFileInternal(outputFileName, null, outputLineDelimiter);
+			} else {
+				computed =
+					readFileInternal(outputFileName, outputFieldDelimiter,
+							outputLineDelimiter);
+			}
+
+			assertTrue(expected.equals(computed));
+
+		} catch (Exception e) {
+			fail(e.toString());
+		} finally {
+			if (inputFile != null) {
+				inputFile.delete();
+				inputFile = null;
+			}
+			if (outputFile != null) {
+				outputFile.delete();
+				outputFile = null;
+			}
+			inputFile = new File(inputFileName);
+			if (inputFile.exists()){
+				inputFile.delete();
+				inputFile = null;
+				inputFileName = null;
+			}
+			outputFile = new File(outputFileName);
+			if (outputFile.exists()){
+				outputFile.delete();
+				outputFile = null;
+				outputFileName = null;
+			}
+		}
 	}
 
 	@Test
 	public void testCreatePartitions() {
-//		fail("Not yet implemented");
+		for (LogPartitionerFileFormat inputFormat : LogPartitionerFileFormat
+				.values()) {
+			for (char inputFieldDelimiter : fieldDelimiters) {
+				for (String inputLineDelimiter : lineDelimiters) {
+					for (LogPartitionerFileFormat outputFormat : LogPartitionerFileFormat
+							.values()) {
+						for (char outputFieldDelimiter : fieldDelimiters) {
+							for (String outputLineDelimiter : lineDelimiters) {
+								testCreatePartitions(inputFormat,
+										inputFieldDelimiter,
+										inputLineDelimiter, outputFormat,
+										outputFieldDelimiter,
+										outputLineDelimiter);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void testWriteReadValueCounts(LogPartitionerFileFormat format,
+			char fieldDelimiter, String lineDelimiter) {
+		File f = null;
+		FileWriter fw = null;
+		try {
+			f = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
+			String fileName = f.getAbsolutePath();
+			f.delete();
+			f = null;
+
+			LogPartitionerApp.writeOutput(valueCounts, fileName, format,
+					fieldDelimiter, lineDelimiter);
+			List<ValueCount> computed =
+				LogPartitionerApp.readInput(fileName, format, fieldDelimiter,
+						lineDelimiter);
+			assertTrue(valueCounts.equals(computed));
+
+		} catch (Exception e) {
+			fail(e.toString());
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					logger.warning(e.toString());
+				}
+				fw = null;
+			}
+			if (f != null) {
+				f.delete();
+				f = null;
+			}
+		}
+	}
+
+	void testWriteReadValueRanks(LogPartitionerFileFormat format,
+			char fieldDelimiter, String lineDelimiter) {
+		File f = null;
+		FileWriter fw = null;
+		try {
+			f = File.createTempFile(TEMP_PREFIX, TEMP_SUFFIX);
+			String fileName = f.getAbsolutePath();
+			f.delete();
+			f = null;
+
+			LogPartitionerApp.writeOutput(expected, fileName, format,
+					fieldDelimiter, lineDelimiter);
+			List<ValueRank> computed;
+			if (format == ALT_LINES) {
+				computed = readFileInternal(fileName, null, lineDelimiter);
+			} else {
+				computed =
+					readFileInternal(fileName, fieldDelimiter, lineDelimiter);
+			}
+			assertTrue(expected.equals(computed));
+
+		} catch (Exception e) {
+			fail(e.toString());
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					logger.warning(e.toString());
+				}
+				fw = null;
+			}
+			if (f != null) {
+				f.delete();
+				f = null;
+			}
+		}
 	}
 
 	@Test
-	public void testReadInput() {
-//		fail("Not yet implemented");
+	public void testWriteReadValueCounts() {
+
+		for (LogPartitionerFileFormat format : LogPartitionerFileFormat
+				.values()) {
+			for (char fieldDelimiter : fieldDelimiters) {
+				for (String lineDelimiter : lineDelimiters) {
+					testWriteReadValueCounts(format, fieldDelimiter,
+							lineDelimiter);
+				}
+			}
+		}
 	}
 
 	@Test
-	public void testWriteOutput() {
-//		fail("Not yet implemented");
+	public void testWriteReadValueRanks() {
+
+		for (LogPartitionerFileFormat format : LogPartitionerFileFormat
+				.values()) {
+			for (char fieldDelimiter : fieldDelimiters) {
+				for (String lineDelimiter : lineDelimiters) {
+					testWriteReadValueCounts(format, fieldDelimiter,
+							lineDelimiter);
+				}
+			}
+		}
 	}
 
 }
--- 2.7.x/misc/logfrequencypartitioner-app/src/test/java/com/choicemaker/cm/logfrequencypartitioner/app/TestUtils.java
+++ 2.7.x/misc/logfrequencypartitioner-app/src/test/java/com/choicemaker/cm/logfrequencypartitioner/app/TestUtils.java
@@ -1,5 +1,17 @@
 package com.choicemaker.cm.logfrequencypartitioner.app;
 
+import static com.choicemaker.util.LogFrequencyPartitioner.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import com.choicemaker.util.LogFrequencyPartitioner.ValueRank;
+
 class TestUtils {
 
 	static final String TEMP_PREFIX = "LogPartitionerTest_";
@@ -10,7 +22,9 @@ class TestUtils {
 		assert !arg.contains(LogPartitionerCommandLine.MAGIC_DIVIDER);
 		String retVal = "";
 		if (isSet) {
-			retVal = LogPartitionerCommandLine.OPTION_FLAG + arg + LogPartitionerCommandLine.MAGIC_DIVIDER;
+			retVal =
+				LogPartitionerCommandLine.OPTION_FLAG + arg
+						+ LogPartitionerCommandLine.MAGIC_DIVIDER;
 		}
 		return retVal;
 	}
@@ -22,10 +36,14 @@ class TestUtils {
 		if (value != null) {
 			if (value.contains(LogPartitionerCommandLine.MAGIC_DIVIDER)) {
 				throw new IllegalArgumentException(
-						"value contains the magic divider ('" + LogPartitionerCommandLine.MAGIC_DIVIDER
+						"value contains the magic divider ('"
+								+ LogPartitionerCommandLine.MAGIC_DIVIDER
 								+ "'): " + value);
 			}
-			retVal = LogPartitionerCommandLine.OPTION_FLAG + arg + LogPartitionerCommandLine.MAGIC_DIVIDER + value + LogPartitionerCommandLine.MAGIC_DIVIDER;
+			retVal =
+				LogPartitionerCommandLine.OPTION_FLAG + arg
+						+ LogPartitionerCommandLine.MAGIC_DIVIDER + value
+						+ LogPartitionerCommandLine.MAGIC_DIVIDER;
 		}
 		return retVal;
 	}
@@ -35,32 +53,124 @@ class TestUtils {
 			throw new IllegalArgumentException(
 					"null log partitioner parameters");
 		}
-	
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(booleanArg(p.isHelp(), LogPartitionerCommandLine.ARG_HELP));
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_INPUT_FILE, p.getInputFileName()));
+		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_INPUT_FILE,
+				p.getInputFileName()));
 		if (p.getInputFormat() != null) {
-			sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_INPUT_FORMAT, p.getInputFormat()
-					.name()));
+			sb.append(argWithQuotedValue(
+					LogPartitionerCommandLine.ARG_INPUT_FORMAT, p
+							.getInputFormat().name()));
 		}
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_INPUT_FIELD_SEP,
+		sb.append(argWithQuotedValue(
+				LogPartitionerCommandLine.ARG_INPUT_FIELD_SEP,
 				String.valueOf(p.getOutputFieldSeparator())));
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_INPUT_LINE_SEP,
+		sb.append(argWithQuotedValue(
+				LogPartitionerCommandLine.ARG_INPUT_LINE_SEP,
 				p.getInputLineSeparator()));
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_OUTPUT_FILE, p.getOutputFileName()));
+		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_OUTPUT_FILE,
+				p.getOutputFileName()));
 		if (p.getOutputFormat() != null) {
-			sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_OUTPUT_FORMAT, p.getOutputFormat()
-					.name()));
+			sb.append(argWithQuotedValue(
+					LogPartitionerCommandLine.ARG_OUTPUT_FORMAT, p
+							.getOutputFormat().name()));
 		}
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_OUTPUT_FIELD_SEP,
+		sb.append(argWithQuotedValue(
+				LogPartitionerCommandLine.ARG_OUTPUT_FIELD_SEP,
 				String.valueOf(p.getOutputFieldSeparator())));
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_OUTPUT_LINE_SEP,
+		sb.append(argWithQuotedValue(
+				LogPartitionerCommandLine.ARG_OUTPUT_LINE_SEP,
 				p.getOutputLineSeparator()));
-		sb.append(argWithQuotedValue(LogPartitionerCommandLine.ARG_PARTITION_COUNT,
+		sb.append(argWithQuotedValue(
+				LogPartitionerCommandLine.ARG_PARTITION_COUNT,
 				String.valueOf(p.getPartitionCount())));
-	
+
 		String s = sb.toString();
 		String[] retVal = s.split(LogPartitionerCommandLine.MAGIC_DIVIDER);
+		return retVal;
+	}
+
+	/**
+	 * A sad example of code copying, because the LogFrequencyPartitioner
+	 * readFile(..) method is not written for reuse (or testing). Until the
+	 * LogFrequencyPartitioner is rewritten, this nearly duplicated code is
+	 * needed.
+	 */
+	static List<ValueRank> readFileInternal(String fileName,
+			Character elementSep, String lineSep) throws IOException {
+		if (fileName == null) {
+			throw new IllegalArgumentException("null file name");
+		}
+		if (lineSep == null) {
+			lineSep = EOL;
+		}
+
+		Pattern p = null;
+		if (elementSep != null) {
+			String sElementSep = String.valueOf(elementSep);
+			String literal = Pattern.quote(sElementSep);
+			p = Pattern.compile(literal);
+		}
+
+		List<ValueRank> retVal = new ArrayList<>();
+		BufferedReader in = null;
+		try {
+			FileReader fr =
+				new FileReader(new File(fileName).getAbsoluteFile());
+			in = new BufferedReader(fr);
+			String line = in.readLine();
+			while (line != null) {
+				String value = null;
+				String sCount = null;
+
+				if (p != null) {
+					// See caveat in LogFrequencyPartitioner.readFile...
+					String[] tokens = p.split(line, -1);
+					if (tokens.length != 2) {
+						String msg = "Invalid line: '" + line + "'";
+						throw new IllegalArgumentException(msg);
+					}
+					value = tokens[0].trim();
+					sCount = tokens[1].trim();
+
+				} else {
+					value = line.trim();
+					sCount = in.readLine();
+					if (sCount == null) {
+						String msg = "Missing count for value '" + value + "'";
+						throw new IllegalArgumentException(msg);
+					} else {
+						sCount = sCount.trim();
+					}
+				}
+
+				int rank = 0;
+				try {
+					rank = Integer.parseInt(sCount);
+				} catch (NumberFormatException x) {
+					String msg =
+						"Invalid count (" + sCount + ") for value '" + value
+								+ "'";
+					throw new IllegalArgumentException(msg);
+				}
+				if (rank < MIN_RANK) {
+					String msg =
+						"Negative rank (" + rank + ") for value '" + value
+								+ "'";
+					throw new IllegalArgumentException(msg);
+				}
+				ValueRank vc = new ValueRank(value, rank);
+				retVal.add(vc);
+
+				line = in.readLine();
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+
 		return retVal;
 	}
 
