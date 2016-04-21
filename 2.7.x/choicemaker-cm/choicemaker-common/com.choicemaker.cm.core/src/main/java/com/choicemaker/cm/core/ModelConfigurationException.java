1452a85956b1db26fc202f594e5c95621c903a13
Rick Hall <rphall@choicemaker.com>
2016-04-21 14:49:21 -0400

    Fixed copyrights in core package

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.core/src/main/java/com/choicemaker/cm/core/ModelConfigurationException.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.core/src/main/java/com/choicemaker/cm/core/ModelConfigurationException.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2016 ChoiceMaker LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package com.choicemaker.cm.core;
 
 public class ModelConfigurationException extends Exception {
