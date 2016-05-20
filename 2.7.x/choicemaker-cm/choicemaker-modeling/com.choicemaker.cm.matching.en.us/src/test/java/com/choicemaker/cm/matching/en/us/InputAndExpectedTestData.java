c96387cb3641378b5e875d0f7ea4da00baa3dd2e
Rick Hall <rphall@choicemaker.com>
2016-05-20 11:10:20 -0400

    Added more test data for NameParser0

--- 2.7.x/choicemaker-cm/choicemaker-modeling/com.choicemaker.cm.matching.en.us/src/test/java/com/choicemaker/cm/matching/en/us/InputAndExpectedTestData.java
+++ 2.7.x/choicemaker-cm/choicemaker-modeling/com.choicemaker.cm.matching.en.us/src/test/java/com/choicemaker/cm/matching/en/us/InputAndExpectedTestData.java
@@ -1,15 +1,70 @@
 package com.choicemaker.cm.matching.en.us;
 
 public class InputAndExpectedTestData {
-	
-	private static final InputAndExpected[] _testData = new InputAndExpected[] {
-		new InputAndExpected("", "", "", new ParsedName0())
-	};
-	
+
+	private static final int IDX_RAW_FN = 0;
+	private static final int IDX_RAW_MN = 1;
+	private static final int IDX_RAW_LN = 2;
+	private static final int IDX_PARSED_FN = 3;
+	private static final int IDX_PARSED_MN = 4;
+	private static final int IDX_PARSED_LN = 5;
+	private static final int IDX_PARSED_TITLES = 6;
+	private static final int IDX_PARSED_MAIDEN = 7;
+	private static final int IDX_PARSED_MOMSFN = 8;
+
+	/**
+	 * Without any collections loaded, the default constructor for NameParser0
+	 * should still handle basic name parsing, including guesses at probable
+	 * mother's maiden names.
+	 */
+	private static final String[][] _testData = new String[][] {
+			new String[] {
+					"", "", "", "", "", "", "", "", "" },
+			new String[] {
+					"XVE", "", "NEEMEN", "XVE", "", "NEEMEN", "", "", "" },
+			new String[] {
+					"XEECEL", "", "NEVEX-GEMEX", "XEECEL", "", "GEMEX", "",
+					"NEVEX", "" },
+			new String[] {
+					"XEERE", "TRE'XER", "NELXEN", "XEERE", "TREXER", "NELXEN",
+					"", "", "" },
+			new String[] {
+					"XXRXEH", "X", "NELXEN", "XXRXEH", "X", "NELXEN", "", "",
+					"" },
+			new String[] {
+					"XEXHEE", "KENGXTEN", "NECHELEX-GEFF", "XEXHEE",
+					"KENGXTEN", "GEFF", "", "NECHELEX", "" },
+			new String[] {
+					"XXRXEH", "XEE'", "NELXEN", "XXRXEH", "XEE", "NELXEN", "",
+					"", "" },
+			new String[] {
+					"XELEN", "DEN-ENTHENX", "NELXEN", "XELEN", "DEN-ENTHENX",
+					"NELXEN", "", "", "" },
+			new String[] {
+					"KXLE", "NGEXEN", "E'XELLEVEN", "KXLE", "NGEXEN",
+					"EXELLEVEN", "", "", "" }, };
+
 	public static InputAndExpected[] getTestData() {
 		final int length = _testData.length;
 		InputAndExpected[] retVal = new InputAndExpected[length];
-		System.arraycopy(_testData, 0, retVal, 0, length);
+		for (int i = 0; i < length; i++) {
+			String RAW_FN = _testData[i][IDX_RAW_FN];
+			String RAW_MN = _testData[i][IDX_RAW_MN];
+			String RAW_LN = _testData[i][IDX_RAW_LN];
+			String PARSED_FN = _testData[i][IDX_PARSED_FN];
+			String PARSED_MN = _testData[i][IDX_PARSED_MN];
+			String PARSED_LN = _testData[i][IDX_PARSED_LN];
+			String PARSED_TITLES = _testData[i][IDX_PARSED_TITLES];
+			String PARSED_MAIDEN = _testData[i][IDX_PARSED_MAIDEN];
+			String PARSED_MOMSFN = _testData[i][IDX_PARSED_MOMSFN];
+			ParsedName0 p = new ParsedName0(PARSED_FN, PARSED_MN, PARSED_LN);
+			p.setMothersFirstName(PARSED_MOMSFN);
+			p.setPotentialMaidenName(PARSED_MAIDEN);
+			p.setTitles(PARSED_TITLES);
+			InputAndExpected iae =
+				new InputAndExpected(RAW_FN, RAW_MN, RAW_LN, p);
+			retVal[i] = iae;
+		}
 		return retVal;
 	}
 
