8601f81be01a522e8261213ebb1a372169998efd
Rick Hall <rphall@choicemaker.com>
2018-02-25 22:34:50 -0500

    Added Javadoc and new, stubbed methods to MergeGroup and TransitivityCandidates

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/MergeGroup.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/MergeGroup.java
@@ -3,8 +3,29 @@ package com.choicemaker.client.api;
 import java.io.Serializable;
 import java.util.List;
 
-public interface MergeCandidates<T extends Comparable<T> & Serializable> extends Serializable {
+public interface MergeGroup<T extends Comparable<T> & Serializable>
+		extends Serializable {
+	
+	/** Returns a unique identifier for the group */
+//	String getMergeGroupId();
+	
+	/** Returns the match connectivity criteria for the group */
 	IGraphProperty getGraphConnectivity();
+
+	/** Returns the records in the merge group */
 	List<DataAccessObject<T>> getRecords();
+
+	/**
+	 * Returns the evaluated match and hold relationships between records
+	 * in the group.
+	 */
 	List<EvaluatedPair<T>> getPairs();
+	
+	/**
+	 * Returns the match or hold relationship between two records in the
+	 * group. If either record is not in the group, an illegal argument
+	 * exception is thrown.
+	 */
+//	EvaluatedPair<T> getEvaluatedPair(DataAccessObject<T> r1, DataAccessObject<T> r2);
+
 }
--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/TransitiveCandidates.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/TransitiveCandidates.java
@@ -5,9 +5,38 @@ import java.util.List;
 
 public interface TransitiveCandidates<T extends Comparable<T> & Serializable> extends Serializable {
 
+	/**
+	 * Returns the query record against which the candidate records are
+	 * compared.
+	 */
 	DataAccessObject<T> getQueryRecord();
-
+	
+	/**
+	 * Returns the EvaluatedPair between the query record and the specified
+	 * candidate record.
+	 */
+//	EvaluatedPair<T> getEvaluatedPair(DataAccessObject<T> candidate);
+	
+	/**
+	 * Returns a list of all EvaluatedPairs between the query record and
+	 * the candidate records.
+	 */
 	List<EvaluatedPair<T>> getEvaluatedPairs();
 
-	List<MergeCandidates<T>> getMergeCandidates();
+	/**
+	 * Returns a list of merge candidates formed by the query record and
+	 * candidate records.
+	 */
+	List<MergeGroup<T>> getMergeCandidates();
+	
+	/**
+	 * Returns the MergeCandidate to which the specified record belongs.
+	 * The specified record may be the query record or any candidate record.
+	 * If the specified record does not belong to a merge candidate, or if
+	 * the specified record is not the query record or a candidate record, a
+	 * null merge candidate is returned.
+	 */
+	MergeGroup<T> getMergeCandidate(DataAccessObject<T> record);
+	
 }
+
