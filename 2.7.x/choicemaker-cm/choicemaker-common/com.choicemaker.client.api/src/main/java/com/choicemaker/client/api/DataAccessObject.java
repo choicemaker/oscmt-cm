6c05ea5e9bbfb4ae88bd94b83aaee3e36cf07b24
Rick Hall <rphall@choicemaker.com>
2018-02-27 02:15:09 -0500

    Fixed comments in client.api DataAccessObject

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/DataAccessObject.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.client.api/src/main/java/com/choicemaker/client/api/DataAccessObject.java
@@ -2,10 +2,10 @@ package com.choicemaker.client.api;
 
 import java.io.Serializable;
 
-/** The interface implemented by generated record-holder classes */
+/**
+ * The interface implemented by generated record-holder classes, which are
+ * serializable and identifiable (with serializable identifiers).
+ */
 public interface DataAccessObject<T extends Comparable<T> & Serializable>
 	extends Identifiable<T>, Serializable {
-	
-//	T getId();
-	
 }
