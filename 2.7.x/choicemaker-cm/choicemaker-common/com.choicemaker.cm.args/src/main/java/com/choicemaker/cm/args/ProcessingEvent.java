60e43190951a43eb19884457211c693046ca6cdc
Rick Hall <rphall@choicemaker.com>
2018-03-15 17:04:50 -0400

    Cleaned up source code for cm.args ProcessingEvent

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.args/src/main/java/com/choicemaker/cm/args/ProcessingEvent.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.args/src/main/java/com/choicemaker/cm/args/ProcessingEvent.java
@@ -3,7 +3,12 @@ package com.choicemaker.cm.args;
 import java.io.Serializable;
 
 public interface ProcessingEvent extends Serializable {
+
+	/** Returns the event name for this entry */
 	String getEventName();
+
 	int getEventId();
+
 	float getPercentComplete();
+
 }
