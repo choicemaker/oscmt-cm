65c8e88e0df369b85c87f54a11821bc8ddadce33
Rick Hall <rphall@choicemaker.com>
2018-10-25 13:23:04 -0400

    Refactored SqlServer record source apps to derive from abstract base class

--- 2.7.x/choicemaker-cmit/cmit-jdbc/com.choicemaker.cmit.io.db.sqlserver/src/main/java/com/choicemaker/cmit/io/db/sqlserver/AbstractSqlServerRecordSourceApp.java
+++ 2.7.x/choicemaker-cmit/cmit-jdbc/com.choicemaker.cmit.io.db.sqlserver/src/main/java/com/choicemaker/cmit/io/db/sqlserver/AbstractSqlServerRecordSourceApp.java
@@ -4,8 +4,6 @@ import static com.choicemaker.cmit.io.db.oracle.JdbcTestUtils.loadProperties;
 import static com.choicemaker.cmit.io.db.oracle.JdbcTestUtils.logProperty;
 import static com.choicemaker.cmit.io.db.sqlserver.C3P0_DataSource.PN_POOLNAME;
 import static com.choicemaker.cmit.io.db.sqlserver.C3P0_DataSource.configureDatasource;
-import static com.choicemaker.cmit.io.db.sqlserver.SqlServerRecordSourceApp.COUNT_RECORDS_BETWEEN_DEBUG_PRINTS;
-import static com.choicemaker.cmit.io.db.sqlserver.SqlServerRecordSourceApp.COUNT_RECORDS_BETWEEN_INFO_PRINTS;
 import static com.choicemaker.cmit.io.db.sqlserver.SqlServerTestProperties.PN_DATABASE_CONFIGURATION;
 import static com.choicemaker.cmit.io.db.sqlserver.SqlServerTestProperties.PN_MODEL_NAME;
 import static com.choicemaker.cmit.io.db.sqlserver.SqlServerTestProperties.PN_SQL_RECORD_SELECTION;
@@ -26,22 +24,33 @@ import com.choicemaker.cm.core.Record;
 import com.choicemaker.cm.core.RecordSource;
 import com.choicemaker.cm.core.base.PMManager;
 import com.choicemaker.cm.io.db.base.DataSources;
-import com.choicemaker.cm.io.db.sqlserver.SqlServerRecordSource;
 import com.choicemaker.util.Precondition;
 
-public class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
+public abstract class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
 
 	private static final Logger log =
 		Logger.getLogger(AbstractSqlServerRecordSourceApp.class.getName());
 
 	private static final String SOURCE = "AbstractSqlServerRecordSourceApp";
 
-	private final String propertyFileName;
-	private final Properties properties;
-	private final String datasourceName;
-	private final String modelName;
-	private final String databaseConfigurationName;
-	private final String sqlRecordSelectionQuery;
+	/** Number of records between when debug print of the current record id */
+	public static final int COUNT_RECORDS_BETWEEN_DEBUG_PRINTS = 5000;
+	/** Number of records between when info print of the current record id */
+	public static final int COUNT_RECORDS_BETWEEN_INFO_PRINTS = 50000;
+
+	public static <T extends Comparable<T>> void logRecord(int count,
+			Record<T> r) {
+		AbstractSqlServerRecordSourceApp.logRecord(count, r,
+				log.isLoggable(Level.FINEST));
+	}
+
+	public static <T extends Comparable<T>> void logRecord(int count,
+			Record<T> r, boolean isLoggable) {
+		if (isLoggable) {
+			String sid = r == null ? null : r.getId().toString();
+			System.err.println(String.format("%8d: %s", count, sid));
+		}
+	}
 
 	public static ImmutableProbabilityModel lookupProbabilityModel(
 			String modelName) throws ModelConfigurationException, IOException {
@@ -58,22 +67,69 @@ public class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
 		return retVal;
 	}
 
-	public static <T extends Comparable<T>> void logRecord(int count,
-			Record<T> r) {
-		SqlServerParallelRecordSourceApp.logRecord(count, r,
-				log.isLoggable(Level.FINEST));
+	private final String propertyFileName;
+
+	private final Properties properties;
+
+	private final String datasourceName;
+
+	private final String modelName;
+
+	private final String databaseConfigurationName;
+
+	private final String sqlRecordSelectionQuery;
+
+	public AbstractSqlServerRecordSourceApp(String _propertyFileName)
+			throws IOException, SQLException, ModelConfigurationException {
+
+		Precondition.assertNonEmptyString("missing name of property file",
+				_propertyFileName);
+		this.propertyFileName = _propertyFileName;
+
+		FileReader fr = new FileReader(propertyFileName);
+		this.properties = loadProperties(fr);
+
+		this.datasourceName = properties.getProperty(PN_POOLNAME);
+		Precondition.assertNonEmptyString("missing name for data source",
+				propertyFileName);
+		logProperty(PN_MODEL_NAME, datasourceName);
+
+		final DataSource ds = configureDatasource(properties);
+		assert ds != null;
+		final DataSource ds2 = DataSources.getDataSource(datasourceName);
+		Precondition.assertBoolean(
+				String.format("misconfigured data source '%s'", datasourceName),
+				ds == ds2);
+
+		this.modelName = properties.getProperty(PN_MODEL_NAME);
+		Precondition.assertNonEmptyString("missing name for model", modelName);
+		logProperty(PN_MODEL_NAME, modelName);
+
+		this.databaseConfigurationName =
+			properties.getProperty(PN_DATABASE_CONFIGURATION);
+		Precondition.assertNonEmptyString("missing database configuration name",
+				databaseConfigurationName);
+		logProperty(PN_DATABASE_CONFIGURATION, databaseConfigurationName);
+
+		this.sqlRecordSelectionQuery =
+			properties.getProperty(PN_SQL_RECORD_SELECTION);
+		Precondition.assertNonEmptyString("missing SQL for record selection",
+				sqlRecordSelectionQuery);
+		logProperty(PN_SQL_RECORD_SELECTION, sqlRecordSelectionQuery);
 	}
 
-	public RecordSource createRecordSource(ImmutableProbabilityModel model)
-			throws ModelConfigurationException, IOException {
-		Precondition.assertNonNullArgument("null model", model);
-
-		final String unusedFileName = null;
-		RecordSource rs =
-			new SqlServerRecordSource(unusedFileName, model, datasourceName,
-					databaseConfigurationName, sqlRecordSelectionQuery);
-		return rs;
-	}
+	public abstract RecordSource createRecordSource(
+			ImmutableProbabilityModel model)
+			throws ModelConfigurationException, IOException;
+	// {
+	// Precondition.assertNonNullArgument("null model", model);
+	//
+	// final String unusedFileName = null;
+	// RecordSource rs =
+	// new SqlServerRecordSource(unusedFileName, model, datasourceName,
+	// databaseConfigurationName, sqlRecordSelectionQuery);
+	// return rs;
+	// }
 
 	public String doDownloadReportStats()
 			throws BlockingException, ModelConfigurationException, IOException {
@@ -113,11 +169,12 @@ public class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
 
 			while (rs.hasNext()) {
 				@SuppressWarnings("unchecked")
-				Record<T> r = (Record<T>) rs.getNext();
+				Record<T> r = rs.getNext();
 				Comparable<T> id = r.getId();
 				boolean isCheckpoint = (retVal
-						% COUNT_RECORDS_BETWEEN_DEBUG_PRINTS == 0)
-						|| (retVal % COUNT_RECORDS_BETWEEN_INFO_PRINTS == 0);
+						% AbstractSqlServerRecordSourceApp.COUNT_RECORDS_BETWEEN_DEBUG_PRINTS == 0)
+						|| (retVal
+								% AbstractSqlServerRecordSourceApp.COUNT_RECORDS_BETWEEN_INFO_PRINTS == 0);
 				if (isCheckpoint) {
 					String msg =
 						String.format("Record %s / count %d", id, retVal);
@@ -145,20 +202,8 @@ public class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
 		return retVal;
 	}
 
-	public static <T extends Comparable<T>> void logRecord(int count,
-			Record<T> r, boolean isLoggable) {
-		if (isLoggable) {
-			String sid = r == null ? null : r.getId().toString();
-			System.err.println(String.format("%8d: %s", count, sid));
-		}
-	}
-
-	public String getPropertyFileName() {
-		return propertyFileName;
-	}
-
-	public Properties getProperties() {
-		return properties;
+	public String getDatabaseConfigurationName() {
+		return databaseConfigurationName;
 	}
 
 	public String getDatasourceName() {
@@ -169,51 +214,16 @@ public class AbstractSqlServerRecordSourceApp<T extends Comparable<T>> {
 		return modelName;
 	}
 
-	public String getDatabaseConfigurationName() {
-		return databaseConfigurationName;
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public String getPropertyFileName() {
+		return propertyFileName;
 	}
 
 	public String getSqlRecordSelectionQuery() {
 		return sqlRecordSelectionQuery;
-	}
-
-	public AbstractSqlServerRecordSourceApp(String _propertyFileName)
-			throws IOException, SQLException, ModelConfigurationException {
-
-		Precondition.assertNonEmptyString("missing name of property file",
-				_propertyFileName);
-		this.propertyFileName = _propertyFileName;
-
-		FileReader fr = new FileReader(propertyFileName);
-		this.properties = loadProperties(fr);
-
-		this.datasourceName = properties.getProperty(PN_POOLNAME);
-		Precondition.assertNonEmptyString("missing name for data source",
-				propertyFileName);
-		logProperty(PN_MODEL_NAME, datasourceName);
-
-		final DataSource ds = configureDatasource(properties);
-		assert ds != null;
-		final DataSource ds2 = DataSources.getDataSource(datasourceName);
-		Precondition.assertBoolean(
-				String.format("misconfigured data source '%s'", datasourceName),
-				ds == ds2);
-
-		this.modelName = properties.getProperty(PN_MODEL_NAME);
-		Precondition.assertNonEmptyString("missing name for model", modelName);
-		logProperty(PN_MODEL_NAME, modelName);
-
-		this.databaseConfigurationName =
-			properties.getProperty(PN_DATABASE_CONFIGURATION);
-		Precondition.assertNonEmptyString("missing database configuration name",
-				databaseConfigurationName);
-		logProperty(PN_DATABASE_CONFIGURATION, databaseConfigurationName);
-
-		this.sqlRecordSelectionQuery =
-			properties.getProperty(PN_SQL_RECORD_SELECTION);
-		Precondition.assertNonEmptyString("missing SQL for record selection",
-				sqlRecordSelectionQuery);
-		logProperty(PN_SQL_RECORD_SELECTION, sqlRecordSelectionQuery);
 	}
 
 }
--- 2.7.x/choicemaker-cmit/cmit-jdbc/com.choicemaker.cmit.io.db.sqlserver/src/main/java/com/choicemaker/cmit/io/db/sqlserver/SqlServerParallelRecordSourceApp.java
+++ 2.7.x/choicemaker-cmit/cmit-jdbc/com.choicemaker.cmit.io.db.sqlserver/src/main/java/com/choicemaker/cmit/io/db/sqlserver/SqlServerParallelRecordSourceApp.java
@@ -5,21 +5,29 @@ import static com.choicemaker.e2.platform.InstallablePlatform.INSTALLABLE_PLATFO
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
+import com.choicemaker.cm.core.ImmutableProbabilityModel;
 import com.choicemaker.cm.core.ModelConfigurationException;
+import com.choicemaker.cm.core.RecordSource;
+import com.choicemaker.cm.io.db.sqlserver.SqlServerParallelRecordSource;
 import com.choicemaker.e2.embed.EmbeddedPlatform;
+import com.choicemaker.util.Precondition;
 import com.choicemaker.util.SystemPropertyUtils;
 
 public class SqlServerParallelRecordSourceApp<T extends Comparable<T>>
 		extends AbstractSqlServerRecordSourceApp<T> {
 
-	public SqlServerParallelRecordSourceApp(String _propertyFileName)
-			throws IOException, SQLException, ModelConfigurationException {
-		super(_propertyFileName);
-	}
+	private static final Logger log =
+		Logger.getLogger(SqlServerParallelRecordSourceApp.class.getName());
+
+	protected static final String SOURCE = "SqlServerParallelRecordSourceApp";
 
 	public static <T extends Comparable<T>> void main(String[] args)
 			throws Exception {
+
+		final String METHOD = "main";
+		log.entering(SOURCE, METHOD, args);
 
 		String pName = EmbeddedPlatform.class.getName();
 		SystemPropertyUtils.setPropertyIfMissing(INSTALLABLE_PLATFORM, pName);
@@ -30,6 +38,25 @@ public class SqlServerParallelRecordSourceApp<T extends Comparable<T>>
 
 		String stats = app.doDownloadReportStats();
 		System.out.println(stats);
+
+		log.exiting(SOURCE, METHOD);
+	}
+
+	public SqlServerParallelRecordSourceApp(String _propertyFileName)
+			throws IOException, SQLException, ModelConfigurationException {
+		super(_propertyFileName);
+	}
+
+	@Override
+	public RecordSource createRecordSource(ImmutableProbabilityModel model)
+			throws ModelConfigurationException, IOException {
+		Precondition.assertNonNullArgument("null model", model);
+
+		final String unusedFileName = null;
+		RecordSource rs = new SqlServerParallelRecordSource(unusedFileName,
+				model, getDatasourceName(), getDatabaseConfigurationName(),
+				getSqlRecordSelectionQuery());
+		return rs;
 	}
 
 }
