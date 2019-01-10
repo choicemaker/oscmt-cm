b213a4635c730c9b2346ce9af381e1b2bb498c6a
Rick Hall <rphall@choicemaker.com>
2019-01-10 16:40:39 -0500

    Working Oracle versions of migrate_to_named-config2 SQL script

--- SQL/oracle/migrate_to_named-config2.sql
+++ SQL/oracle/migrate_to_named-config2.sql
@@ -1,124 +1,119 @@
-USE [EPI_MPIMODELING]
-GO
-
-INSERT INTO [dbo].[CMT_NAMED_CONFIG2]
-           ([NC_ID]
-           ,[ABA_BLOCKSET]
-           ,[ABA_SINGLESET]
-           ,[ABA_MATCHES]
-           ,[ABA_SINGLETABLE]
-           ,[BLOCKING_CONF]
-           ,[NC_DESC]
-           ,[NC_NAME]
-           ,[DATA_SOURCE]
-           ,[HIGH_THRESHOLD]
-           ,[CLASS]
-           ,[LOW_THRESHOLD]
-           ,[MODEL]
-           ,[OABA_INTERVAL]
-           ,[OABA_BLOCKSIZE]
-           ,[OABA_CHUNKSIZE]
-           ,[OABA_MATCHES]
-           ,[OABA_OVERSIZE]
-           ,[OABA_SINGLE]
-           ,[OABA_OSFIELDS]
-           ,[OPTLOCK]
-           ,[QUERY_DBCONF]
-           ,[QUERY_RS_DEDUPED]
-           ,[QUERY_SQL]
-           ,[RS_TYPE]
-           ,[REF_DBACC]
-           ,[REF_DBCONF]
-           ,[REF_DBREAD]
-           ,[REF_SQL]
-           ,[RIGOR]
-           ,[FILE_URI]
-           ,[SVR_FILES_COUNT]
-           ,[SVR_FILE_ENTRIES]
-           ,[SVR_THREADS]
-           ,[TASK]
-           ,[FORMAT]
-           ,[GRAPH]
-           ,[UUID])
+INSERT INTO "CMT_NAMED_CONFIG2"
+           ("NC_ID"
+           ,"ABA_BLOCKSET"
+           ,"ABA_SINGLESET"
+           ,"ABA_MATCHES"
+           ,"ABA_SINGLETABLE"
+           ,"BLOCKING_CONF"
+           ,"NC_DESC"
+           ,"NC_NAME"
+           ,"DATA_SOURCE"
+           ,"HIGH_THRESHOLD"
+           ,"CLASS"
+           ,"LOW_THRESHOLD"
+           ,"MODEL"
+           ,"OABA_INTERVAL"
+           ,"OABA_BLOCKSIZE"
+           ,"OABA_CHUNKSIZE"
+           ,"OABA_MATCHES"
+           ,"OABA_OVERSIZE"
+           ,"OABA_SINGLE"
+           ,"OABA_OSFIELDS"
+           ,"OPTLOCK"
+           ,"QUERY_DBCONF"
+           ,"QUERY_RS_DEDUPED"
+           ,"QUERY_SQL"
+           ,"RS_TYPE"
+           ,"REF_DBACC"
+           ,"REF_DBCONF"
+           ,"REF_DBREAD"
+           ,"REF_SQL"
+           ,"RIGOR"
+           ,"FILE_URI"
+           ,"SVR_FILES_COUNT"
+           ,"SVR_FILE_ENTRIES"
+           ,"SVR_THREADS"
+           ,"TASK"
+           ,"FORMAT"
+           ,"GRAPH"
+           ,"UUID")
 SELECT
-    -- <NC_ID, numeric(19,0),>
-       [NC_ID]
-    -- <ABA_BLOCKSET, int,>
-      ,[LIMIT_BLOCKSET]
-    -- <ABA_SINGLESET, int,>
-      ,[LIMIT_SINGLESET]
-    -- <ABA_MATCHES, int,>
-      ,[ABA_MAX_MATCHES]
-    -- <ABA_SINGLETABLE, int,>
-      ,[LIMIT_SINGLETABLE]
-    -- <BLOCKING_CONF, varchar(255),>
-      ,[BLOCKING_CONF]
-    -- <NC_DESC, varchar(255),>
-      ,[NC_DESC]
-    -- <NC_NAME, varchar(255),>
-      ,[NC_NAME]
-    -- <DATA_SOURCE, varchar(255),>
-      ,[DATA_SOURCE]
-    -- <HIGH_THRESHOLD, real,>
-      ,[HIGH_THRESHOLD]
-    -- <CLASS, varchar(255),>
-      ,[CLASS]
-    -- <LOW_THRESHOLD, real,>
-      ,[LOW_THRESHOLD]
-    -- <MODEL, varchar(255),>
-      ,[MODEL]
-    -- <OABA_INTERVAL, int,>
-      ,[INTERVAL]
-    -- <OABA_BLOCKSIZE, int,>
-      ,[MAX_BLOCKSIZE]
-    -- <OABA_CHUNKSIZE, int,>
-      ,[MAX_CHUNKSIZE]
-    -- <OABA_MATCHES, int,>
-      ,[MAX_MATCHES]
-    -- <OABA_OVERSIZE, int,>
-      ,[MAX_OVERSIZE]
-    -- <OABA_SINGLE, int,>
-      ,[MAX_SINGLE]
-    -- <OABA_OSFIELDS, int,>
-      ,[MIN_FIELDS]
-    -- <OPTLOCK, int,>
-      ,[OPTLOCK]
-    -- <QUERY_DBCONF, varchar(255),>
-      ,[QUERY_DBCONF]
-    -- <QUERY_RS_DEDUPED, bit,>
-      ,[QUERY_RS_DEDUPED]
-    -- <QUERY_SQL, varchar(255),>
-      ,[QUERY_SQL]
-    -- <RS_TYPE, varchar(255),>
-      ,[RS_TYPE]
-    -- <REF_DBACC, varchar(255),>
-      ,[REF_DBACC]
-    -- <REF_DBCONF, varchar(255),>
-      ,[REF_DBCONF]
-    -- <REF_DBREAD, varchar(255),>
-      ,[REF_DBREAD]
-    -- <REF_SQL, varchar(255),>
-      ,[REF_SQL]
-    -- <RIGOR, varchar(255),>
-      ,[RIGOR]
-    -- <FILE_URI, varchar(255),>
-      ,[FILE_URI]
-    -- <SVR_FILES_COUNT, int,>
-      ,[MAX_CHUNK_COUNT]
-    -- <SVR_FILE_ENTRIES, int,>
-      ,[MAX_CHUNK_SIZE]
-    -- <SVR_THREADS, int,>
-      ,[MAX_THREADS]
-    -- <TASK, varchar(255),>
-      ,[TASK]
-    -- <FORMAT, varchar(255),>
-      ,[FORMAT]
-    -- <GRAPH, varchar(255),>
-      ,[GRAPH]
-    -- <UUID, varchar(255),>
-      ,[UUID]
-  FROM [dbo].[CMT_NAMED_CONFIG]
-GO
-
-
+    -- <"NC_ID", number(19,0)>
+       "NC_ID"
+    -- <"ABA_BLOCKSET", number(10,0)>
+      ,"LIMIT_BLOCKSET"
+    -- <"ABA_SINGLESET", number(10,0)>
+      ,"LIMIT_SINGLESET"
+    -- <"ABA_MATCHES", number(10,0)>
+      ,"ABA_MAX_MATCHES"
+    -- <"ABA_SINGLETABLE", number(10,0)>
+      ,"LIMIT_SINGLETABLE"
+    -- <"BLOCKING_CONF", varchar2(255)>
+      ,"BLOCKING_CONF"
+    -- <"NC_DESC", varchar2(255)>
+      ,"NC_DESC"
+    -- <"NC_NAME", varchar2(255)>
+      ,"NC_NAME"
+    -- <"DATA_SOURCE", varchar2(255)>
+      ,"DATA_SOURCE"
+    -- <"HIGH_THRESHOLD", float>
+      ,"HIGH_THRESHOLD"
+    -- <"CLASS", varchar2(255)>
+      ,"CLASS"
+    -- <"LOW_THRESHOLD", float>
+      ,"LOW_THRESHOLD"
+    -- <"MODEL", varchar2(255)>
+      ,"MODEL"
+    -- <"OABA_INTERVAL", number(10,0)>
+      ,"INTERVAL"
+    -- <"OABA_BLOCKSIZE", number(10,0)>
+      ,"MAX_BLOCKSIZE"
+    -- <"OABA_CHUNKSIZE", number(10,0)>
+      ,"MAX_CHUNKSIZE"
+    -- <"OABA_MATCHES", number(10,0)>
+      ,"MAX_MATCHES"
+    -- <"OABA_OVERSIZE", number(10,0)>
+      ,"MAX_OVERSIZE"
+    -- <"OABA_SINGLE", number(10,0)>
+      ,"MAX_SINGLE"
+    -- <"OABA_OSFIELDS", number(10,0)>
+      ,"MIN_FIELDS"
+    -- <"OPTLOCK", number(10,0)>
+      ,"OPTLOCK"
+    -- <"QUERY_DBCONF", varchar2(255)>
+      ,"QUERY_DBCONF"
+    -- <"QUERY_RS_DEDUPED", number(1,0)>
+      ,"QUERY_RS_DEDUPED"
+    -- <"QUERY_SQL", varchar2(255)>
+      ,"QUERY_SQL"
+    -- <"RS_TYPE", varchar2(255)>
+      ,"RS_TYPE"
+    -- <"REF_DBACC", varchar2(255)>
+      ,"REF_DBACC"
+    -- <"REF_DBCONF", varchar2(255)>
+      ,"REF_DBCONF"
+    -- <"REF_DBREAD", varchar2(255)>
+      ,"REF_DBREAD"
+    -- <"REF_SQL", varchar2(255)>
+      ,"REF_SQL"
+    -- <"RIGOR", varchar2(255)>
+      ,"RIGOR"
+    -- <"FILE_URI", varchar2(255)>
+      ,"FILE_URI"
+    -- <"SVR_FILES_COUNT", number(10,0)>
+      ,"MAX_CHUNK_COUNT"
+    -- <"SVR_FILE_ENTRIES", number(10,0)>
+      ,"MAX_CHUNK_SIZE"
+    -- <"SVR_THREADS", number(10,0)>
+      ,"MAX_THREADS"
+    -- <"TASK", varchar2(255)>
+      ,"TASK"
+    -- <"FORMAT", varchar2(255)>
+      ,"FORMAT"
+    -- <"GRAPH", varchar2(255)>
+      ,"GRAPH"
+    -- <"UUID", varchar2(255)>
+      ,"UUID"
+  FROM "CMT_NAMED_CONFIG"
+;
 
