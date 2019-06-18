df0c167e93deb00419da77808296197770176e44
Rick Hall <rphall@choicemaker.com>
2019-06-18 18:29:49 -0400

    Remove unused static member N_FIELD from aba.base.Field

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.aba.base/src/main/java/com/choicemaker/cm/aba/base/Field.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.aba.base/src/main/java/com/choicemaker/cm/aba/base/Field.java
@@ -20,7 +20,6 @@ public abstract class Field implements Serializable, IField {
 
 	private static final long serialVersionUID = 271;
 
-	protected static final Field[] N_FIELD = new Field[0];
 	protected static final Field[][] NN_FIELD = new Field[0][0];
 
 	private IField[][] illegalCombinations;
