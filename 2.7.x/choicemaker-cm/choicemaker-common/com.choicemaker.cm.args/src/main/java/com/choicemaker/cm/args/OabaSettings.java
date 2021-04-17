4226f28eaa97ef8e32a8332f10929cbc60c81436
rphall <rphall@choicemaker.com>
2021-04-17 09:44:31 -0400

    Rollback commit ca3e89f01504d5 of OabaSettings.java
    Defer API extension until version 2.7.2

--- 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.args/src/main/java/com/choicemaker/cm/args/OabaSettings.java
+++ 2.7.x/choicemaker-cm/choicemaker-common/com.choicemaker.cm.args/src/main/java/com/choicemaker/cm/args/OabaSettings.java
@@ -14,9 +14,9 @@ package com.choicemaker.cm.args;
  * Settings for the Offline Automated Batch Algorithm (OABA) are matching
  * parameters that change relatively infrequently. Prior to version 2.7 of
  * ChoiceMaker, these settings were stored with each instance of a model
- * configuration. However, since these settings rarely change, and are often
+ * configuration. However, since these statistics rarely change, and are often
  * reused between jobs, it makes more sense to store them separately from any
- * particular model. In a J2EE environment, OABA settings are cached in memory
+ * particular model. In a J2EE environment, OABA statistics are cached in memory
  * by an EJB singleton service.
  * 
  * @author rphall
@@ -31,18 +31,16 @@ public interface OabaSettings extends AbaSettings {
 	int DEFAULT_MAX_MATCHES = 500000;
 	int DEFAULT_MIN_FIELDS = 3;
 	int DEFAULT_INTERVAL = 100;
-	boolean DEFAULT_USE_DATABASE_RECORD_ID_TRANSLATIONS = false;
 
 	/**
-	 * @return The maximum size of a 'regular' OABA blocking set. Similar to the
-	 *         ABA {@link #getLimitPerBlockingSet() blocking set limit}.
+	 * @return The maximum size of a 'regular' OABA blocking set. Similar to the ABA
+	 * {@link #getLimitPerBlockingSet() blocking set limit}.
 	 */
 	int getMaxBlockSize();
 
 	/**
 	 * The maximum number of records to pack into a chunk file. Essentially an
 	 * indirect bound on the file size of a chunk.
-	 * 
 	 * @return The maximum number of records to pack into a chunk file
 	 */
 	int getMaxChunkSize();
@@ -54,7 +52,6 @@ public interface OabaSettings extends AbaSettings {
 	 * of fields specified by {@link #getMinFields()}, the blocking set will be
 	 * kept if it contains fewer than this limit. If these conditions are not
 	 * met, the blocking set is discarded.
-	 * 
 	 * @return The maximum size of an 'oversized' blocking set
 	 */
 	int getMaxOversized();
@@ -63,7 +60,6 @@ public interface OabaSettings extends AbaSettings {
 	 * The maximum number of matches to package into an indexed pair-wise result
 	 * file. After the maximum is reached, a new indexed file is started with an
 	 * index incremented by 1 (one).
-	 * 
 	 * @return The maximum number of matches
 	 */
 	int getMaxMatches();
@@ -71,7 +67,6 @@ public interface OabaSettings extends AbaSettings {
 	/**
 	 * The minimum number of fields from which an oversized blocking set must be
 	 * formed in order for it to be kept for further processing.
-	 * 
 	 * @return The minimum number of fields
 	 */
 	int getMinFields();
@@ -81,37 +76,20 @@ public interface OabaSettings extends AbaSettings {
 	 * records is being processed in a loop, this parameter sets the frequency
 	 * at which the loop checks to see if processing should continue or whether
 	 * processing should be discontinued.
-	 * 
 	 * @return the polling interval
 	 */
 	int getInterval();
 
 	/**
-	 * The threshold number of records beyond which query records are matched in
-	 * a batch. Below this threshold, query records are matched to the reference
-	 * records one at a time. Usually set to zero (0) so that batch processing
-	 * is always performed, but should be set to a value larger than the number
-	 * of query records if there are only a few thousand query record and the
-	 * number of reference records is a few million or more.
-	 * 
+   * The threshold number of records beyond which query records are matched
+   * in a batch. Below this threshold, query records are matched to the
+   * reference records one at a time. Usually set to zero (0) so that
+	 * batch processing is always performed, but should be set to a value
+	 * larger than the number of query records if there are only a few
+	 * thousand query record and the number of reference records is a
+	 * few million or more.
 	 * @return the batch processing threshold
 	 */
 	int getMaxSingle();
-
-	/**
-	 * To save memory, record ids are translated to int-valued aliases when
-	 * blocking values are computed. The most general method is to compute
-	 * translations on the fly, as records are retrieved from a data source.
-	 * This is the default method used by the OABA service. A considerable
-	 * savings in time can be achieved, however, when the record source is a
-	 * relational database and a <code>DbRecordIdTranslatorFactory</code> has
-	 * been configured. This flag indicates whether database translations are
-	 * available and should be used by the OABA service. (This flag is
-	 * irrelevant to the Transitivity service.)
-	 * 
-	 * @return true if database translations are available and should be used by
-	 *         the OABA service when blocking values are computed.
-	 */
-	boolean useDatabaseRecordIdTranslations();
 
 }
