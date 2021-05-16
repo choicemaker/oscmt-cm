e54d7fc66041692ecf8a6892fb4f1920e77f6dd9
rphall <rphall@choicemaker.com>
2021-05-16 12:54:19 -0400

    Add matching.geo.GeoUtil.distance(lat1,lat2,long1,long2) method

--- 2.7.x/choicemaker-cm/choicemaker-modeling/com.choicemaker.cm.matching.geo/src/main/java/com/choicemaker/cm/matching/geo/GeoUtil.java
+++ 2.7.x/choicemaker-cm/choicemaker-modeling/com.choicemaker.cm.matching.geo/src/main/java/com/choicemaker/cm/matching/geo/GeoUtil.java
@@ -3,18 +3,21 @@ package com.choicemaker.cm.matching.geo;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.choicemaker.util.Precondition;
+
 public class GeoUtil {
 
 	private static final Logger logger =
 		Logger.getLogger(GeoUtil.class.getName());
+
+	private static final String msg0 =
+		"latitude1: %f, latitude2: %f, longitude1: %f, longitude2: %f, miles: %f";
 
 	/** Earth's mean radius in miles (6371 km) */
 	public static double EARTH_RADIOUS = 3963.1;
 
 	/** 1 mile = 1.609344 kilometers */
 	public static double KM_PER_MILE = 1.609344;
-
-	private static double RPG = Math.PI / 180.00;
 
 	@Deprecated
 	public static double RPG_INT = Math.PI / 1800000.00;
@@ -42,53 +45,79 @@ public class GeoUtil {
 	}
 
 	/**
-	 * Calculates distance between two geo-points using Haversine formula.
+	 * Calculates distance in miles between two geo-points using Haversine
+	 * formula.
+	 * 
+	 * @param latitude1,
+	 *            in degrees, between -180f and +180f
+	 * @param latitude2,
+	 *            in degrees, between -180f and +180f
+	 * @param longitude1,
+	 *            in degrees, between -90f and +90f
+	 * @param lon2,
+	 *            in degrees, between -90f and +90f
+	 * @return distance in miles
+	 */
+	public static double distance(double latitude1, double latitude2,
+			double longitude1, double longitude2) {
+		Precondition.assertBoolean(
+				"latitude1 must be between -180.0 and +180.0, inclusive",
+				latitude1 >= -180.0 && latitude1 <= 180.0);
+		Precondition.assertBoolean(
+				"latitude2 must be between -180.0 and +180.0, inclusive",
+				latitude2 >= -180.0 && latitude2 <= 180.0);
+		Precondition.assertBoolean(
+				"longitude1 must be between -90.0 and +90.0, inclusive",
+				longitude1 >= -90.0 && longitude1 <= 90.0);
+		Precondition.assertBoolean(
+				"longitude2 must be between -90.0 and +90.0, inclusive",
+				longitude2 >= -90.0 && longitude2 <= 90.0);
+
+		longitude1 = Math.toRadians(longitude1);
+		longitude2 = Math.toRadians(longitude2);
+		latitude1 = Math.toRadians(latitude1);
+		latitude2 = Math.toRadians(latitude2);
+
+		// Haversine formula
+		double dlon = longitude2 - longitude1;
+		double dlat = latitude2 - latitude1;
+		double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(latitude1)
+				* Math.cos(latitude2) * Math.pow(Math.sin(dlon / 2), 2);
+
+		double c = 2 * Math.asin(Math.sqrt(a));
+
+		double retVal = (c * EARTH_RADIOUS);
+
+		if (logger.isLoggable(Level.FINE)) {
+			String msg = String.format(msg0, latitude1, latitude2, longitude1,
+					longitude2, retVal);
+			logger.fine(msg);
+		}
+
+		return retVal;
+	}
+
+	/**
+	 * Calculates distance in miles between two geo-points using Haversine
+	 * formula.
 	 * 
 	 * @param gp1
 	 *            First geo-point.
 	 * @param gp2
 	 *            Second geo-point.
-	 * @return Distance.
+	 * @return distance in miles, or -1.0 if either argument is null
 	 */
 	public static double dist(GeoPoint gp1, GeoPoint gp2) {
-		if (gp1 == null || gp2 == null)
-			return -1;
 
-		final double dLat = (gp2.latitude - gp1.latitude) * RPG;
-		final double dLon = (gp2.longitude - gp1.longitude) * RPG;
-		final double lat1 = gp1.latitude * RPG;
-		final double lat2 = gp2.latitude * RPG;
-		final double a =
-			Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1)
-					* Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-		final double dist =
-			EARTH_RADIOUS * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-		// Trick to set flag if assertion is enabled
-		boolean assertOn = false;
-		assert assertOn = true;
-
-		// If assertion or logging is enabled, compare to deprecated method
-		if (logger.isLoggable(Level.WARNING) || assertOn) {
-			double PRECISION = 0.002; // 0.002 miles == 10 feet
-			double _dLat = (gp2.lat - gp1.lat) * RPG_INT;
-			double _dLon = (gp2.lon - gp1.lon) * RPG_INT;
-			double _lat1 = gp1.lat * RPG_INT;
-			double _lat2 = gp2.lat * RPG_INT;
-			double _a = Math.sin(_dLat / 2) * Math.sin(_dLat / 2)
-					+ Math.cos(_lat1) * Math.cos(_lat2) * Math.sin(_dLon / 2)
-							* Math.sin(_dLon / 2);
-			final double _dist = EARTH_RADIOUS * 2
-					* Math.atan2(Math.sqrt(_a), Math.sqrt(1 - _a));
-			if (Math.abs(_dist - dist) > PRECISION) {
-				String msg = "Revised geo-distance computation (" + dist
-						+ ") != deprecrated computation (" + _dist + ")";
-				logger.warning(msg);
-				assert false : msg;
-			}
+		double retVal;
+		if (gp1 == null || gp2 == null) {
+			retVal = -1d;
+		} else {
+			retVal = distance(gp1.latitude, gp2.latitude, gp1.longitude,
+					gp2.longitude);
 		}
 
-		return dist;
+		return retVal;
 	}
 
 }
