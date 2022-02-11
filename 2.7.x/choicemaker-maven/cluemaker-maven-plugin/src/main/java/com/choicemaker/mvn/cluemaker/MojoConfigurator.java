f2a52364b2134cedccf29636f98d87e88ee55215
rphall <rphall@choicemaker.com>
2022-02-11 14:50:41 -0500

    Update copyrights in choicemaker-maven

--- 2.7.x/choicemaker-maven/cluemaker-maven-plugin/src/main/java/com/choicemaker/mvn/cluemaker/MojoConfigurator.java
+++ 2.7.x/choicemaker-maven/cluemaker-maven-plugin/src/main/java/com/choicemaker/mvn/cluemaker/MojoConfigurator.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2016 ChoiceMaker LLC and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ChoiceMaker LLC - initial API and implementation
+ *******************************************************************************/
 package com.choicemaker.mvn.cluemaker;
 
 import java.io.File;
