0b45984de61272e5a06039e49116c23084a0cb18
rphall <rphall@choicemaker.com>
2022-02-12 10:52:59 -0500

    Update copyrights in SecondString library

--- 2.7.x/choicemaker-cm/choicemaker-modeling/com.wcohen.ss/src/main/java/com/wcohen/ss/AbstractTokenizedStringDistance.java
+++ 2.7.x/choicemaker-cm/choicemaker-modeling/com.wcohen.ss/src/main/java/com/wcohen/ss/AbstractTokenizedStringDistance.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2003 Carnegie Mellon University
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of an instance of the University of Illinois/NCSA Open Source
+ * license which accompanies this distribution.
+ *
+ * Authors: William W. Cohen, Pradeep Ravikumar, Stephen E. Fienberg, and others
+ * https://sourceforge.net/projects/secondstring
+ *******************************************************************************/
 package com.wcohen.ss;
 
 import java.util.ArrayList;
