0256a5fedbf9879535b61c8155772117d7b4d70a
rphall <rphall@choicemaker.com>
2022-02-13 00:08:07 -0500

    Update copyrights in ChoiceMaker j2ee libraries

--- 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.transitivity.ejb/src/main/java/com/choicemaker/cm/transitivity/ejb/TransitivityBatchControllerBean.java
+++ 2.7.x/choicemaker-cm/choicemaker-j2ee/com.choicemaker.cm.transitivity.ejb/src/main/java/com/choicemaker/cm/transitivity/ejb/TransitivityBatchControllerBean.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2020 ChoiceMaker LLC and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ChoiceMaker Technologies, Inc  - initial API and implementation
+ *     ChoiceMaker LLC - version 2.7 and following
+ *******************************************************************************/
 package com.choicemaker.cm.transitivity.ejb;
 
 import java.io.IOException;
